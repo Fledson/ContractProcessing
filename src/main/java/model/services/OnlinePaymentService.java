f658432cb7d6919f7b1efdd441c5bd6351ce2aa6
Fledson Henrique <fledsonhenrique@gmail.com>
2024-02-17 20:27:44 -0300

    ✨ feat: adicionando variaveis de juros a interface

--- src/main/java/model/services/OnlinePaymentService.java
+++ src/main/java/model/services/OnlinePaymentService.java
@@ -1,6 +1,9 @@
 package model.services;
 
 public interface OnlinePaymentService {
+    double monthlyInterest = 0;
+    double paymentFee = 0;
+
     Double interest(Double amount, Integer months);
     Double paymentFee(Double amount);
 }
