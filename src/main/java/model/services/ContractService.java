e0519b6fe484b46218c7cede5246cd901c659059
Fledson Henrique <fledsonhenrique@gmail.com>
2024-02-17 20:30:59 -0300

    ✨ feat: implementando calculos de juros e taxas nas parcelas e colocando a injeção e inversão de dependencia no OnlinePaymentService

--- src/main/java/model/services/ContractService.java
+++ src/main/java/model/services/ContractService.java
@@ -4,13 +4,12 @@ import model.entites.Contract;
 import model.entites.Installment;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class ContractService {
 
 
-    public void processContract(Contract contract, Integer months){
+    public void processContract(Contract contract, Integer months, OnlinePaymentService paymentService){
         // definindo um calaendario
         GregorianCalendar calendar = new GregorianCalendar();
         calendar.setTime(contract.getDate());
@@ -18,15 +17,18 @@ public class ContractService {
 
 
         // para cada mes, calcular a data da parcela e o valor
-        for (int parcela = 1; parcela <= months; parcela ++) {
+        for (int installment = 1; installment <= months; installment ++) {
             // Incrementando um mês na data do contrato
             calendar.add(Calendar.MONTH, 1);
 
-            // valor
+            // calculando valor da parcela
             double valueInstallment = contract.getTotalValue() / months;
-
-            // instanciando
-            contract.getInstallments().add(new Installment(calendar.getTime(), valueInstallment));
+            // calculando o valor do juros simples da parcela e somando a ela
+            double valueWithSimpleInterest = valueInstallment + paymentService.interest(valueInstallment, installment);
+            // calculando o valor da taxa de pagamento em cima do valor da parcela com o juros simples e somando
+            double amountWithPaymentFee = valueWithSimpleInterest + paymentService.paymentFee(valueWithSimpleInterest);
+            // instanciando e adicionando as parcelas a lista de parcelas
+            contract.getInstallments().add(new Installment(calendar.getTime(), amountWithPaymentFee));
         }
     }
 
